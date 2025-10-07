22357a46e6ad0fd38209792be47760f38047c947
Keifer Erikson <keifer@erikson.dev>
2025-10-07 14:46:32 -0600

    WIP: progress on topnavbar component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import TopNavbar from '@/components/TopNavbar';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopNavbar />
         {children}
       </body>
     </html>
